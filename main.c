6141fad9f314f68ba05e8bf85a8b2202de1abfdb
Nathan Henrique <nathanlucindosantos@gmail.com>
2023-11-25 10:03:04 -0300

    dando uma adicionada

--- main.c
+++ main.c
@@ -1,23 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct instrucoes{
-    
-    int opcode;    
+struct Mips
+{
+    char opcode[10];
     int rs;
     int rt;
     int rd;
-    int shamt;
+    int shift;
     int funct;
-    int offset
-} ;
+    int offset;
+};
+
+int registradores[32];
+
+void processarInstrucao(struct Mips *instrucao)
+{
+
+    printf("Opcode: %s\n", instrucao->opcode);
+    printf("rs: %d\n", instrucao->rs);
+    printf("rt: %d\n", instrucao->rt);
+    printf("rd: %d\n", instrucao->rd);
+    printf("shamt: %d\n", instrucao->shift);
+    printf("funct: %d\n", instrucao->funct);
+    printf("offset: %d\n", instrucao->offset);
+
+    if (strcmp(instrucao->opcode, "add") == 0)
+    {
+        
+    }
+    else if (strcmp(instrucao->opcode, "sub") == 0)
+    {
+
+    }
+}
 
 int main()
 {
-    int pc = 0;
-    int registradores[32] = {0}; // Registradores
-    int memoria[1024] = {0}; // Memória 
+    struct Mips instrucao;
 
+    char escolha;
+
+    do
+    {
+
+        printf("Digite a instrucao MIPS (por exemplo, ADD $s1, $s2, $s3): ");
+        scanf("%s $%d , $%d , $%d", instrucao.opcode, &instrucao.rs, &instrucao.rt, &instrucao.rd);
+
+        processarInstrucao(&instrucao);
+
+        while (getchar() != '\n')
+            ;
+
+        printf("Deseja adicionar mais instrucoes? (s para sim, qualquer outra tecla para sair): ");
+        scanf(" %c", &escolha);
+
+    } while (escolha == 's' || escolha == 'S');
 
     return 0;
 }
